9b736d40e8530f9247d889145e47a9b271a88caa
Joann Ellis <joannpellis@gmail.com>
2024-06-21 17:02:00 -0400

    debugging

---
+++ debugging/script.js
@@ -0,0 +1,49 @@
+const storeOwners = [
+    {
+      name: 'Danny Shavez',
+      stores: 1,
+      location: 'AZ',
+    },
+    {
+      name: 'Danny Shavez',
+      stores: 1,
+      location: 'NM',
+    },
+    {
+      name: 'Danny Shavez',
+      stores: 1,
+      location: 'NM',
+    },
+    {
+      name: 'Danny Shavez',
+      stores: 1,
+      location: 'NM',
+    },
+  ];
+  
+  const listNumberOfStores = function () {  
+    let totalLocations = 0;                                      
+    for (let i = 0; i < storeOwners.length; i++) {
+      totalLocations += storeOwners[i].stores; 
+    }
+    return totalLocations;
+  };
+  
+  let locations = listNumberOfStores();
+   
+  function tellMeMyStores() {
+    console.log('Hey, can you tell me how many stores you have?');
+    if (locations > 0) {
+      console.log('Of course, we have ' + locations + ' stores');
+    }
+  }
+  
+  function hasStore() {
+    for (let i = 0; i < storeOwners.length; i++) {
+      console.log(`Yes, ${storeOwners[i].name} has one in ${storeOwners[i].location}`);
+    }
+  
+  }
+  
+  tellMeMyStores();
+  hasStore();
